4ebff311c88c5f826737674f296e9d0344c10430
ray846 <rayli777+trend@gmail.com>
2021-01-05 23:23:17 +0800

    Add lineCount

---
+++ cmd/linecount.go
@@ -0,0 +1,74 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// linecountCmd represents the linecount command
+var linecountCmd = &cobra.Command{
+	Use:     "linecount",
+	Short:   "Print the line count of file",
+	RunE:    lineCount,
+	Example: "  linecount -f [file]",
+}
+var file string
+
+func init() {
+	rootCmd.AddCommand(linecountCmd)
+	linecountCmd.Flags().StringVarP(&file, "file", "f", "", " input file")
+	linecountCmd.MarkFlagRequired("file")
+}
+
+func lineCount(cmd *cobra.Command, args []string) error {
+	//TODO check non-exist-file, got directory, binary file
+	fileContent, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+
+	const lineBreak = '\n'
+	count := 1 //first line
+	buf := make([]byte, bufio.MaxScanTokenSize)
+	for {
+		bufferSize, err := fileContent.Read(buf)
+		if err != nil && err != io.EOF {
+			return err
+		}
+
+		var buffPosition int
+		for {
+			i := bytes.IndexByte(buf[buffPosition:], lineBreak)
+			if i == -1 || bufferSize == buffPosition {
+				break
+			}
+			buffPosition += i + 1
+			count++
+		}
+		if err == io.EOF {
+			break
+		}
+	}
+	fmt.Print(count)
+	return nil
+}
